3b87386ae2d1e9a2ea2362c3727a8fb479ab3326
dbogatyreev <den.bogatyrev.09@mail.ru>
2024-08-12 01:21:45 +0300

    Add new changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Counter from './components/Counter';
+import ClassCounter from './components/ClassCounter';
+import './styles/App.css';
+import PostItem from './components/Postitem';
+import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
+import PostForm from './components/PostForm';
 
 function App() {
-  const [value, setValue] = useState('Текс в инпуте')
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Javascript', body: 'Description' },
+    { id: 2, title: 'Javascript 2', body: 'Description' },
+    { id: 3, title: 'Javascript 3', body: 'Description' },
+  ])
 
- 
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+  }
+
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
   return (
     <div className="App">
-
-      <Counter/>
+      <PostForm create={createPost} />
+      {posts.length
+        ?
+        <PostList remove={removePost} posts={posts} title="Посты про JS" />
+        :
+        <h1 style={{ textAlign: 'center' }}>
+          Посты не найдены!
+        </h1>
+      }
 
     </div>
   );
---
+++ src/components/PostList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import PostItem from "./Postitem"
+
+const PostList = ({ posts, title, remove }) => {
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: 'center' }}>
+        {title}
+      </h1>
+      {posts.map((post, index) =>
+        <PostItem remove={remove} number={index + 1} post={post} key={post.id} />
+      )}
+    </div>
+  );
+};
+
+export default PostList;
